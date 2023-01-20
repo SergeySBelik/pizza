2cac0fc6953f327a9ec0901c007f7af835ebc000
SergeySBelik <sergey.s.belik89@gmail.com>
2023-01-20 17:47:15 +0200

    Routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react'
 import './scss/app.scss'
 import Header from './components/Header'
 import Home from './pages/Home'
+import Cart from './pages/Cart'
+import NotFound from './pages/NotFound'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
@@ -9,7 +12,11 @@ function App() {
       <Header />
       <div className="content">
         <div className="container">
-          <Home />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
         </div>
       </div>
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,21 @@
+import { Link } from 'react-router-dom'
 import imgLogo from '../assets/img/pizza-logo.svg'
 
 function Header() {
   return (
     <div className="header">
       <div className="container">
-        <div className="header__logo">
-          <img width="38" src={imgLogo} alt="Pizza logo" />
-          <div>
-            <h1>React Pizza</h1>
-            <p>самая вкусная пицца во вселенной</p>
+        <Link to="/">
+          <div className="header__logo">
+            <img width="38" src={imgLogo} alt="Pizza logo" />
+            <div>
+              <h1>React Pizza</h1>
+              <p>самая вкусная пицца во вселенной</p>
+            </div>
           </div>
-        </div>
-
+        </Link>
         <div className="header__cart">
-          <a className="button button--cart">
+          <Link to="/cart" className="button button--cart">
             <span>520 ₽</span>
             <div className="button__delimiter"></div>
             <svg
@@ -46,7 +48,7 @@ function Header() {
               />
             </svg>
             <span>3</span>
-          </a>
+          </Link>
         </div>
       </div>
     </div>
