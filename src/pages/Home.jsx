4938ba3503031719a54d87210b35de16e52841ad
SergeySBelik <sergey.s.belik89@gmail.com>
2023-01-20 17:20:32 +0200

    Sceleton

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,20 @@ import React, { useEffect, useState } from 'react'
 import Categories from '../components/Categories'
 import Sort from '../components/Sort'
 import PizzaBlock from '../components/PizzaBlock'
+import Sceleton from '../components/Sceleton'
 // import pizzas from '../assets/pizzas.json'
 function Home() {
   const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
   useEffect(() => {
+    setIsLoading(true)
     fetch(`https://63c12a607165626718742aea.mockapi.io/items`)
       .then((res) => res.json())
-      .then((arr) => setItems(arr))
+      .then((arr) => {
+        setItems(arr)
+        setIsLoading(false)
+        window.scrollTo(0, 0)
+      })
   }, [])
 
   return (
@@ -19,9 +26,9 @@ function Home() {
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
-        {items.map((obj, i) => (
-          <PizzaBlock key={i} {...obj} />
-        ))}
+        {isLoading
+          ? [...new Array(6)].map((_, index) => <Sceleton key={index} />)
+          : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
       </div>
     </div>
   )
